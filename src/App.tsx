d7e3b1ec4efea8aa399412bd98bceaa1585da1c5
João Vieira <netojoao599@gmail.com>
2025-09-02 20:54:39 -0300

    Estrutura HTML e CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,39 @@
 import './styles/theme.css';
 import './styles/global.css';
 
-import { Heading } from './components/Heading';
-import { TimerIcon } from 'lucide-react';
-
-
 
 export function App() {;
 
   return (
     <>
-      <Heading>
-        React 
-        <button>
-          <TimerIcon />
-        </button>
-      </Heading>
-      <p>
-        Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown 
-        printer took a galley of type and scrambled it to make a type specimen book.
-      </p>
+      <div className="container">
+        <div className="content">
+          <section>
+            LOGO
+          </section>
+        </div>
+      </div>
+      <div className="container">
+        <div className="content">
+          <section>
+           MENU
+          </section>
+        </div>
+      </div>
+      <div className="container">
+        <div className="content">
+          <section>
+           FORM
+          </section>
+        </div>
+      </div>
+      <div className="container">
+        <div className="content">
+          <section>
+           FOOTER
+          </section>
+        </div>
+      </div>
     </>
   )
 }
